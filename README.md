1f0123a09cc4704f6de526223991a310f9b2319b
Ronik Bhaskar <ronik.r.bhaskar@gmail.com>
2022-08-19 20:27:58 -0500

    I'll do it myself.

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Text Generation: Web
 
+To see the original text generator written in Python, go here: https://github.com/ronikbhaskar/markov_text_generation 
+
+It has far more formatting functionality and offers greater control to the user.
+
 Originally, this project was to enable beginners to build interesting, first web apps. That phase of the project seems to be over, but I've gotten requests and interest from artists to have increased functionality within the app. Since they don't know Python, this website is the only way they have access to my technology. As a result, I've decided to take on my own challenge, building a powerful, easy-to-use web app that gives users access to the many features available in `markov.js`. 
 
 Design Plan:
-There is one, central window for text entry and text output. Above the window will be a text input for naming models. Next to that will be a number input for the number of tokens to generate. Above that text input will be 5 buttons: "Generate Model from Corpus",  "Generate Model from JSON", "Generate Text", "Generate Model JSON", and "Delete Model". Below the window will be a dynamic list of buttons corresponding to the generated models. Clicking a button will select that model for whichever action you choose. You can select as many models as you want to do any given task that requires model selection. If no models are selected for an action that requires models to be selected, nothing happens. If you are trying to generate a model from JSON that doesn't parse, nothing happens. Any text in the window will be overwritten if the action generates text. There is no undo to any of this. Seems harsh, I know, but it keeps it simple. The model naming feature is already a lot of flexibility to the user. 
+There is one, central window for text entry and text output. Above the window will be a text input for naming models. Next to that will be a number input for the number of tokens to generate. Above that text input will be 5 buttons: "Generate Model from Corpus",  "Generate Model from JSON", "Generate Text", "Generate Model JSON", and "Delete Model". Below the window will be a dynamic list of buttons corresponding to the generated models. Clicking a button will select that model for whichever action you choose. You can select as many models as you want to do any given task that requires model selection. If no models are selected for an action that requires models to be selected, nothing happens. If you are trying to generate a model from JSON that doesn't parse, nothing happens. Any text in the window will be overwritten if the action generates text. There is no undo to any of this. Seems harsh, I know, but it keeps it simple. 
 
 ```
             (Generate Model from Corpus)   (Generate Text)            (Delete Model(s))
@@ -20,7 +24,7 @@ There is one, central window for text entry and text output. Above the window wi
             |                                                                         |
             |_________________________________________________________________________|
             (My Homework) (The Art of War) (Chat History) (HIMYM) 
-            (Wikipedia Page on the Troubador)
+            (Wikipedia Page on the Troubadour)
 ```
 
 ---
--- frontend.js
+++ frontend.js
@@ -1,32 +1,243 @@
 /*
  * This is where you can go absolutely wild with the frontend design
- * Author: INSERT NAME HERE
+ * Author: Ronik Bhaskar
  */
 
-/* Example code (it works as is, but it's boring to look at) */
-const myInputWindow = document.getElementById("myInputWindow");
-const myOutput = document.getElementById("myOutput");
-const myNumInput = document.getElementById("myNumInput");
+const modelButtons = document.getElementById("models");
+const textArea = document.getElementById("textArea");
+const modelName = document.getElementById("modelName");
+const numTokens = document.getElementById("numTokens");
+const nameModelMap = new Map();
 
-const myButton = document.getElementById("myButton");
-function onMyButtonClick() 
+textArea.focus();
+
+function genModelFromCorpus()
 {
-    /* reads in text and generates output*/
-    const text = myInputWindow.value.trim();
+    const text = textArea.value.trim();
+
     if (text.length === 0)
-    {
-        console.log("ERROR: no input text");
         return;
+
+    let name = modelName.value.trim();
+    
+    if (name.length === 0)
+        name = "Model";
+
+    if (nameModelMap.has(name))
+        name = name + " 2";
+
+    let digitsToRemove;
+    for (let i = 3; nameModelMap.has(name); ++i)
+    {
+        digitsToRemove = (i-1).toString().length;
+        name = name.slice(0, -digitsToRemove) + i;
     }
+    
     const markov = new AssociationTable();
     const tokens = cleanText(text); 
     markov.train(tokens);
     markov.updateProbabilities();
-    const numOutput = Number(myNumInput.value);
+
+    nameModelMap.set(name, markov);
+
+    const modelButton = document.createElement("button");
+    modelButton.classList.add("model-button");
+    modelButton.innerText = name;
+    modelButton.onclick = genModelButtonFunction(name);
+    modelButtons.appendChild(modelButton);
+}
+
+function genModelButtonFunction(name)
+{
+    return function onModelButtonClick()
+    {
+        const len = modelButtons.children.length;
+        for (let i = 0; i < len; ++i)
+        {
+            if (name === modelButtons.children[i].innerText)
+            {
+                modelButtons.children[i].classList.toggle("selected");
+                break;
+            }
+        }
+    }
+}
+
+function getSelectedModels()
+{
+    let selectedModels = [];
+    const len = modelButtons.children.length;
+    for (let i = 0; i < len; ++i)
+    {
+        /* innerText matches the key for a model. push that model */
+        if (modelButtons.children[i].classList.contains("selected"))
+            selectedModels.push(nameModelMap.get(modelButtons.children[i].innerText))
+    }
+
+    return selectedModels;
+}
+
+function genTextFromModels()
+{
+    const numOutput = Number(numTokens.value);
+
+    if (numOutput === 0)
+        return;
+
+    const selectedModels = getSelectedModels();
+
+    if (selectedModels.length === 0)
+        return;
+
+    let markov = new AssociationTable();
+
+    selectedModels.forEach(model =>
+    {
+        markov = markov.combine(model);
+    });
+
+    markov.updateProbabilities();
+
     const output = markov.genText(numOutput);
 
-    /* sets output */
-    myOutput.innerText = output;
+    textArea.value = output;
 }
-myButton.addEventListener("click", onMyButtonClick);
-/* end example code */
+
+function deleteModels()
+{
+    let names = [];
+    let children = [];
+    const len = modelButtons.children.length;
+    for (let i = 0; i < len; ++i)
+    {
+        /* innerText matches the key for a model. push that model */
+        if (modelButtons.children[i].classList.contains("selected"))
+        {
+            child = modelButtons.children[i];
+            children.push(child);
+            names.push(child.innerText);
+        }
+    }
+
+    if (names.length === 0)
+        return;
+
+    children.forEach(child =>
+    {
+        modelButtons.removeChild(child);
+    });
+
+    names.forEach(name =>
+    {
+        nameModelMap.delete(name);
+    });
+}
+
+function genJSONFromModels()
+{
+    const selectedModels = getSelectedModels();
+
+    if (selectedModels.length === 0)
+        return;
+
+    let markov = new AssociationTable();
+
+    selectedModels.forEach(model =>
+    {
+        markov = markov.combine(model);
+    });
+
+    textArea.value = markov.makeCopyable();
+
+}
+
+function genModelFromJSON()
+{
+    const text = textArea.value.trim();
+
+    if (text.length === 0)
+        return;
+
+    let name = modelName.value.trim();
+    
+    if (name.length === 0)
+        name = "Model";
+
+    if (nameModelMap.has(name))
+        name = name + " 2";
+
+    let digitsToRemove;
+    for (let i = 3; nameModelMap.has(name); ++i)
+    {
+        digitsToRemove = (i-1).toString().length;
+        name = name.slice(0, -digitsToRemove) + i;
+    }
+    
+    const markov = AssociationTable.fromCopyable(text);
+    
+    if (!markov)
+        return;
+
+    markov.updateProbabilities();
+
+    nameModelMap.set(name, markov);
+
+    const modelButton = document.createElement("button");
+    modelButton.classList.add("model-button");
+    modelButton.innerText = name;
+    modelButton.onclick = genModelButtonFunction(name);
+    modelButtons.appendChild(modelButton);
+}
+
+function createTags(input) {
+    const tags = input.split(",").filter(tag => tag.trim() !== "").map(tag => tag.trim())
+    
+    tagsEl.innerHTML = ""
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement("span")
+        tagEl.classList.add("tag")
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
+    })
+}
+
+function randomSelect() {
+    const times = 30
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+	
+	if (randomTag !== undefined) {
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100)
+	}
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100)
+
+    }, times * 100)
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll(".tag")
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add("highlight")
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove("highlight")
+}
